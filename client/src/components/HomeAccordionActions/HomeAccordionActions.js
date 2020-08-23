8a6fc647f8d1cf3c52c9e0fc694ebd452bc0d13f
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-08-23 03:21:16 -0700

    Added HomeAccordionActions component

---
+++ client/src/components/HomeAccordionActions/HomeAccordionActions.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import HomeContext from '../../utils/HomeContext'
+
+const HomeAccordionActions = props => {
+  const { handleArchiveTask, handleDeleteTask } = useContext(HomeContext)
+
+  return (
+    <>
+      <Button
+        size="small"
+        variant="contained"
+        color="primary"
+        startIcon={<EditIcon />}
+        onClick={() => {
+          // Add edit task dialog
+        }}
+      >
+        Edit
+      </Button>
+      <Button
+        size="small"
+        variant="contained"
+        startIcon={<ArchiveIcon />}
+        onClick={handleArchiveTask(props._id)}
+      >
+        Archive
+      </Button>
+      <DeleteTaskDialog 
+        title={"Delete " + props.title} 
+        _id={props._id} 
+        handleDeleteTask={handleDeleteTask} 
+      />
+    </>
+  )
+}
+
+export default HomeAccordionActions
