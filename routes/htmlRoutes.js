bf7055bbfdd1badb77a6e4db2cf00a54fcee6556
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-08-31 00:45:09 -0700

    Added console log to html catch all route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const { join } = require('path')
 module.exports = app => {
 
   app.get('*', (req, res) => {
-    console.log(req)
+    console.log(req.headers.referer)
     res.send(join(__dirname, "client", "build"))
   })
   
